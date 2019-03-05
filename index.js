8476262c15630c0cf6906fb96f178575b793d19f
Blake A Long <blake.a.long@gmail.com>
2019-03-05 21:15:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var facts = ["He was the last Beatle to learn to drive",
              "He was a choir boy and boy scout",
              "He hated the sound of his own voice" ];
 
-function johnLennonFacts(facts) {
+var johnLennonFacts = facts => {
   var i = 0;
   while ( i < facts.length) {
     facts[i] += "!!!";
